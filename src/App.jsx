e346bac94083ce5ef0853a9515f6959fbbe46e00
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-04-14 14:23:04 +0500

    responsiv

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
 import { Toaster } from "react-hot-toast";
 import TeacherLayout from "./components/layout/TeacherLayout";
 import Login from "./pages/auth/Login";
@@ -16,6 +15,7 @@ import TeacherCourseDetail from "./pages/teacher/CourseDetail";
 import SavedCourses from "./pages/student/SavedCourses";
 import EditCourse from "./pages/teacher/EditCourse";
 import EditLesson from "./pages/teacher/EditLesson";
+import { store } from "./store/store";
 function App() {
   return (
     <Provider store={store}>
