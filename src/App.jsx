5bbc2607b0241963d51aac8ab43e0ae1bc94095a
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-04-14 14:39:48 +0500

    sdas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+
 import { Toaster } from "react-hot-toast";
 import TeacherLayout from "./components/layout/TeacherLayout";
 import Login from "./pages/auth/Login";
@@ -18,50 +17,48 @@ import EditCourse from "./pages/teacher/EditCourse";
 import EditLesson from "./pages/teacher/EditLesson";
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Toaster
-          position="top-center"
-          toastOptions={{
-            duration: 3000,
-            style: {
-              fontFamily: "sans-serif",
-            },
-            success: {
-              className: "border-l-4 border-green-500",
-            },
-            error: {
-              className: "border-l-4 border-red-500",
-            },
-          }}
-        />
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+    <BrowserRouter>
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            fontFamily: "sans-serif",
+          },
+          success: {
+            className: "border-l-4 border-green-500",
+          },
+          error: {
+            className: "border-l-4 border-red-500",
+          },
+        }}
+      />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-          <Route element={<ProtectedRoute roles={["student"]} />}>
-            <Route path="/student" element={<StudentLayout />}>
-              <Route path="courses" element={<StudentCourses />} />
-              <Route path="courses/:id" element={<CourseDetail />} />
-              <Route path="saved" element={<SavedCourses />} />
-              <Route path="profile" element={<Profile />} />
-            </Route>
+        <Route element={<ProtectedRoute roles={["student"]} />}>
+          <Route path="/student" element={<StudentLayout />}>
+            <Route path="courses" element={<StudentCourses />} />
+            <Route path="courses/:id" element={<CourseDetail />} />
+            <Route path="saved" element={<SavedCourses />} />
+            <Route path="profile" element={<Profile />} />
           </Route>
+        </Route>
 
-          <Route element={<ProtectedRoute roles={["teacher"]} />}>
-            <Route path="/teacher" element={<TeacherLayout />}>
-              <Route index element={<TeacherCourses />} />
-              <Route path="courses/:id" element={<TeacherCourseDetail />} />
-              <Route path="courses/:id/edit" element={<EditCourse />} />
-              <Route path="lessons/:id/edit" element={<EditLesson />} />
-              <Route path="profile" element={<Profile />} />
-            </Route>
+        <Route element={<ProtectedRoute roles={["teacher"]} />}>
+          <Route path="/teacher" element={<TeacherLayout />}>
+            <Route index element={<TeacherCourses />} />
+            <Route path="courses/:id" element={<TeacherCourseDetail />} />
+            <Route path="courses/:id/edit" element={<EditCourse />} />
+            <Route path="lessons/:id/edit" element={<EditLesson />} />
+            <Route path="profile" element={<Profile />} />
           </Route>
+        </Route>
 
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
