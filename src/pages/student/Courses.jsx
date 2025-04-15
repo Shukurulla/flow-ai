1c529ce0f348e29c70ce37dc54d92770741b3aa0
Shukurulla <shukurullatursinbayev978@gmail.com>
2025-04-16 02:55:23 +0500

    finally project finished

--- src/pages/student/Courses.jsx
+++ src/pages/student/Courses.jsx
@@ -60,7 +60,7 @@ const StudentCourses = () => {
 
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6 text-gray-800">All Courses</h1>
+      <h1 className="text-2xl font-bold mb-6 text-gray-800">Barcha kurslar</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {courses?.map((course) => (
           <CourseCard
